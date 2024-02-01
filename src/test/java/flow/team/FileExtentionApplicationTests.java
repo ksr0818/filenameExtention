1cf403c746d787e86b5fcfd7ebf53eac1105012f
ksr0818 <ksr940818@gmail.com>
2024-02-01 19:39:41 +0900

    [feat] 실행파일 및 설치파일 commit

---
+++ src/test/java/flow/team/FileExtentionApplicationTests.java
@@ -0,0 +1,13 @@
+package flow.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileExtentionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
